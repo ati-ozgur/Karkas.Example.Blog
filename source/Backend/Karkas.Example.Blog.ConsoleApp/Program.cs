f1805dc2a757a93e93ed53ff87b0e4dfaf8c1984
Atilla Ozgur <ati.ozgur@gmail.com>
2022-05-31 21:57:59 +0300

    update running code

--- source/Backend/Karkas.Example.Blog.ConsoleApp/Program.cs
+++ source/Backend/Karkas.Example.Blog.ConsoleApp/Program.cs
@@ -19,21 +19,21 @@ string connectionString = "Data Source=localhost;Initial Catalog=BLOG;Persist Se
 
 ConnectionSingleton.Instance.AddConnection("Blog", dbProviderName, connectionString);
 
+Guid UsersKey = new Guid("6C21D4CC-6FE7-464D-8968-5D73B08820A6");
 
-
-//Users u1 = new Users();
+Users u1 = new Users();
 
 //// set properties here
 
-//u1.UsersKey = Guid.NewGuid();
-//u1.UserName = "Atilla";
-//u1.Email = "atilla@example.com";
-//u1.UsersTypeNo = 1;
+u1.UsersKey = UsersKey;
+u1.UserName = "Atilla";
+u1.Email = "atilla@example.com";
+u1.UsersTypeNo = 1;
 
 
-//UsersDal usersDal = new UsersDal();
+UsersDal usersDal = new UsersDal();
 
-//usersDal.Insert(u1);
+usersDal.Insert(u1);
 
 
 BlogPost bp = new BlogPost();
@@ -41,8 +41,8 @@ bp.BlogPostKey = Guid.NewGuid();
 bp.PostText = "Example text";
 bp.BlogTitle = "Title Console";
 bp.DateCreated = DateTime.Now;
-bp.UsersKey = new Guid("6C21D4CC-6FE7-464D-8968-5D73B08820A6");
-bp.BlogTypeNo = 5;
+bp.UsersKey = UsersKey;
+bp.BlogTypeNo = 2;
 
 BlogPostDal blogPostDal = new BlogPostDal();
 blogPostDal.Insert(bp);
